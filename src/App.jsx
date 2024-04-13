f3e4122bb9cada4cf17a593849590e96da32769f
AdnanAlezari <adnan30062000@gmail.com>
2024-04-13 15:00:57 +0200

    added header and main sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/header/Header'
+import MainProduct from './components/mainProduct/MainProduct';
 function App() {
 
   return (
     <>
     <Header/>
+    <MainProduct/>
     </>
   )
 }
---
+++ src/components/mainProduct/MainProduct.jsx
@@ -0,0 +1,19 @@
+export default function MainProduct (){
+      return(
+            <div className="mainProduct">
+                  <div className="container">
+                  <div className="main-title__wrapper">
+                        <h1 className="main__header">Tech Heim</h1>
+                        <h3 className="main__subtitle">"Join the <span>digital revolution</span>"</h3>
+                        <button className="main__button">Explore more</button>
+                  </div>
+                  <div className="main-img__wrapper">
+                        <img className="main-img" src="/src/img/main-img.png" alt="" />
+                  </div>
+                  </div>
+
+            </div>
+            
+
+      )
+}
