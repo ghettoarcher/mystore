c2fadbc11825c17c375a7efa342ad78ea1876aff
ghettoarcher <adnan30062000@gmail.com>
2024-05-12 16:12:48 +0200

    replaced jpg to svg in bestseller section

--- src/components/bestsellers/BestSellers.jsx
+++ src/components/bestsellers/BestSellers.jsx
@@ -5,28 +5,28 @@ export default function Bestsellers(){
       const bestsellersData = [
             {
                   id:1,
-                    img: '/src/img/bestsellers/razer.jpg',
+                    img: '/src/img/bestsellers/razer.svg',
                     name:'Iphone 14 promax 256GB',
                     price:'$930.00',
                     rating:'4.9',  
                 },
                 { 
                       id:2,
-                      img: '/src/img/bestsellers/playstation.jpg',
+                      img: '/src/img/bestsellers/playstation4.svg',
                       name:'Iphone 14 promax 256GB',
                       price:'$930.00',
                       rating:'4.9',  
                   },
                   {
                       id:3,
-                      img: '/src/img/bestsellers/laptop.png',
+                      img: '/src/img/bestsellers/laptop.svg',
                       name:'Iphone 14 promax 256GB',
                       price:'$930.00',
                       rating:'4.9',  
                   },
                   {
                       id:4,
-                      img: '/src/img/bestsellers/airpods.png',
+                      img: '/src/img/bestsellers/airpods.svg',
                       name:'Iphone 14 promax 256GB',
                       price:'$930.00',
                       rating:'4.9',  
