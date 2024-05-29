9b571bec8041d4b181f9dd3c1f575493188aa4f4
ghettoarcher <adnan30062000@gmail.com>
2024-05-29 12:25:19 +0200

    Revert "refactor search-menu"
    This reverts commit 9992bbc128fb01654956b7bf172b297955892136.

--- src/components/header/SearchMenu/SearchMenu.jsx
+++ src/components/header/SearchMenu/SearchMenu.jsx
@@ -2,10 +2,10 @@ const SearchMenu = () =>{
       
       return(
             <>
-                  <div className="search-menu rounded-2 position-absolute z-1">
+                  <div className="search-menu">
 
-                  <div className="search-menu-input__wrapper d-flex flex-row justify-content-between">
-                        <div className="search-menu__input d-flex flex-row justify-content-between align-items-center rounded-2 p-3">
+                  <div className="search-menu-input__wrapper">
+                        <div className="search-menu__input">
                               <input type="text" placeholder='What we can help you to find?' />
                               <img src="src/icons/header/search.svg" alt="search" />
                         </div>
@@ -14,11 +14,11 @@ const SearchMenu = () =>{
 
                   </div>
 
-                  <div className="search-menu__keywords d-flex flex-row align-items-start justify-content-start pt-5">
+                  <div className="search-menu__keywords">
                         <div className="keywords__most-searched">
                               <p>The most searched Items</p>
-                              <div className="keywords__wrapper d-flex flex-row">
-                              <div className="keywords_most_searched__items d-flex flex-column">
+                              <div className="keywords__wrapper">
+                              <div className="keywords_most_searched__items">
                                     <a href="#"className="keywords_most_searched__item">Macbook Pro</a>
                                     <a href="#"className="keywords_most_searched__item">AirPods Pro</a>
                                     <a href="#"className="keywords_most_searched__item">Samsung S9</a>
