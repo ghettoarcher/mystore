a677ba9ba67a8c427f7e37bbdc71687f31151b45
ghettoarcher <adnan30062000@gmail.com>
2024-06-10 17:34:10 +0200

    added responsive behavior to services section

--- src/components/blogs/BlogList.jsx
+++ src/components/blogs/BlogList.jsx
@@ -7,10 +7,10 @@ export default function BlogList({data}){
                   <div className="blog-item-content d-flex flex-row">
                         <a href=""></a>
                   <img className="blog-item-img" src="/src/img/blogs/blog-image1.png" alt="" />
-                        <div className="blog-item-info d-flex flex-column">
+                        <div className="blog-item-info d-flex flex-column ">
                               <div className="blog-item-title"><a href=""></a>{item.title}</div>
                               <div className="blog-item-descr fw-light mt-2">{item.description}</div>
-                              <div className="blog-item-date d-flex flex-row align-items-center date">
+                              <div className="blog-item-date d-flex flex-row align-items-center date pt-4">
                                     <img src="/src/assets/icons/calendar.svg" alt="date" className="blog-item-date-icon" />
                                     <div className="blog-item-date-text fw-light ">{item.date}</div>
                               </div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,20 +2,20 @@ export default function Services(){
       return(
           <section className="services">
               <div className="container">
-                  <div className="services-content d-flex flex-row justify-content-between">
-                      <div className="services-item">
+                  <div className="services-content row">
+                      <div className="services-item col-xl-3 col-md-6 col-sm-12">
                           <div className="services-icon"><img src="src/assets/icons/tech-icon.svg" alt="" /></div>
                           <div className="services-text">Latest and Greatest Tech</div>
                       </div>
-                      <div className="services-item">
+                      <div className="services-item col-xl-3 col-md-6 col-sm-12">
                           <div className="services-icon"><img src="src/assets/icons/guarantee-icon.svg" alt="guarantee" /></div>
                           <div className="services-text">Guarantee</div>
                       </div>
-                      <div className="services-item">
+                      <div className="services-item col-xl-3 col-md-6 col-sm-12">
                           <div className="services-icon"><img src="src/assets/icons/shipping-icon.svg" alt="shipping" /></div>
                           <div className="services-text">Free Shipping over 1000$</div>
                       </div>
-                      <div className="services-item">
+                      <div className="services-item col-xl-3 col-md-6 col-sm-12">
                           <div className="services-icon"><img src="src/assets/icons/support-icon.svg" alt="support" /></div>
                           <div className="services-text">24/7 Support</div>
                       </div>
