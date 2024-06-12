b961f1295866589d2e0311928ab2d8957de4a99a
AdnanAlezari <adnan30062000@gmail.com>
2024-06-12 15:07:29 +0200

    added primary and secondary colors variables

--- src/components/mainProduct/MainProduct.jsx
+++ src/components/mainProduct/MainProduct.jsx
@@ -8,7 +8,7 @@ export default function MainProduct (){
                         <div className="main-product-content d-flex flex-row justify-content-between">
                               <div className="main-title__wrapper d-flex flex-column justify-content-around">
                                     <h1 className="main__header">Tech Heim</h1>
-                                    <h3 className="main__subtitle">&quot; Join the <span className="text-primary">digital revolution &quot;</span></h3>
+                                    <h3 className="main__subtitle">&quot; Join the <span className="secondary-color">digital revolution &quot;</span></h3>
                                     <div className="main__button fs-6 fw-normal rounded-3 d-flex justify-content-center align-items-center b-none text-center"><a href="">Explore more</a></div>
                               </div>
                         <img className="main-img" src="/src/img/main/main-product.svg" alt="" />
